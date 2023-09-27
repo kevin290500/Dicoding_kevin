e00853a76c0042d13e149b7fd8e328bdfb434e3a
kevin290500 <87468062+kevin290500@users.noreply.github.com>
2023-09-27 09:20:27 +0700

    Add files via upload

---
+++ dashboard.py
@@ -0,0 +1,221 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from babel.numbers import format_currency
+sns.set(style='dark')
+# all_df = pd.read_csv("submission/Dashboard/all_data.csv")
+all_df = pd.read_csv("all_data.csv")
+#=======================================================================================
+def create_daily_orders_df(df):
+    daily_orders_df = df.resample(rule='D', on='order_purchase_timestamp').agg({
+        "order_id": "nunique",
+        "payment_value": "sum"
+    })
+    daily_orders_df = daily_orders_df.reset_index()
+    daily_orders_df.rename(columns={
+        "order_id": "order_count",
+        "payment_value": "revenue"
+    }, inplace=True)
+    
+    return daily_orders_df
+
+def create_sum_order_items_df(df):
+    sum_order_items_df = df.groupby("product_category_name_english")['payment_value'].sum().sort_values(ascending=False).reset_index()
+    return sum_order_items_df
+
+def create_bystate_df(df):
+    bystate_df = df.groupby(by="customer_state").customer_id.nunique().reset_index()
+    bystate_df.rename(columns={
+        "customer_id": "customer_count"
+    }, inplace=True)
+    
+    return bystate_df
+
+def sum_tipe(df):
+    sum_df = df.groupby("payment_type")['payment_value'].sum().sort_values(ascending=False).reset_index()
+    return sum_df
+
+def create_rfm_df(df):
+    rfm_df = df.groupby(by="customer_id", as_index=False).agg({
+        "order_purchase_timestamp": "max", #mengambil tanggal order terakhir
+        "customer_unique_id": "nunique",
+        "payment_value": "sum"
+    })
+    rfm_df.columns = ["customer_id", "max_order_timestamp", "frequency", "monetary"]
+    
+    rfm_df["max_order_timestamp"] = rfm_df["max_order_timestamp"].dt.date
+    recent_date = df["order_purchase_timestamp"].dt.date.max()
+    rfm_df["recency"] = rfm_df["max_order_timestamp"].apply(lambda x: (recent_date - x).days)
+    rfm_df.drop("max_order_timestamp", axis=1, inplace=True)
+    
+    return rfm_df
+
+#=======================================================================================
+st.header('E-Commerce Analysis :sparkles:')
+
+datetime_columns = ["order_purchase_timestamp"]
+all_df.sort_values(by="order_purchase_timestamp", inplace=True)
+all_df.reset_index(inplace=True)
+ 
+for column in datetime_columns:
+    all_df[column] = pd.to_datetime(all_df[column])
+
+min_date = all_df["order_purchase_timestamp"].min()
+max_date = all_df["order_purchase_timestamp"].max()
+
+with st.sidebar:
+    # Menambahkan logo perusahaan
+    st.image("https://github.com/dicodingacademy/assets/raw/main/logo.png")
+    
+    # Mengambil start_date & end_date dari date_input
+    start_date, end_date = st.date_input(
+        label='Rentang Waktu',min_value=min_date,
+        max_value=max_date,
+        value=[min_date, max_date]
+    )
+main_df = all_df[(all_df["order_purchase_timestamp"] >= str(start_date)) & 
+                (all_df["order_purchase_timestamp"] <= str(end_date))]
+
+#=======================================================================================
+
+#Function Waktu
+daily_orders_df = create_daily_orders_df(main_df)
+sum_order_items_df = create_sum_order_items_df(main_df)
+bystate_df = create_bystate_df(main_df)
+sum_df = sum_tipe(main_df)
+rfm_df = create_rfm_df(main_df)
+
+#=======================================================================================
+
+#Daily Orders
+st.subheader('Daily Orders')
+
+col1, col2 = st.columns(2)
+ 
+with col1:
+    total_orders = daily_orders_df.order_count.sum()
+    st.metric("Total orders", value=total_orders)
+ 
+with col2:
+    total_revenue = format_currency(daily_orders_df.revenue.sum(), "AUD", locale='es_CO') 
+    st.metric("Total Revenue", value=total_revenue)
+ 
+fig, ax = plt.subplots(figsize=(16, 8))
+ax.plot(
+    daily_orders_df["order_purchase_timestamp"],
+    daily_orders_df["order_count"],
+    marker='o', 
+    linewidth=2,
+    color="#90CAF9"
+)
+ax.tick_params(axis='y', labelsize=20)
+ax.tick_params(axis='x', labelsize=15)
+ 
+st.pyplot(fig)
+
+#=======================================================================================
+
+#SUM ORDER
+st.subheader("Best & Worst Performing Product")
+ 
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(35, 15))
+ 
+colors = ["#90CAF9", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+ 
+sns.barplot(x="payment_value", y="product_category_name_english", data=sum_order_items_df.head(5), palette=colors, ax=ax[0])
+ax[0].set_ylabel(None)
+ax[0].set_xlabel("Number of Sales", fontsize=30)
+ax[0].set_title("Best Performing Product", loc="center", fontsize=50)
+ax[0].tick_params(axis='y', labelsize=35)
+ax[0].tick_params(axis='x', labelsize=30)
+ 
+sns.barplot(x="payment_value", y="product_category_name_english", data=sum_order_items_df.sort_values(by="payment_value", ascending=True).head(5), palette=colors, ax=ax[1])
+ax[1].set_ylabel(None)
+ax[1].set_xlabel("Number of Sales", fontsize=30)
+ax[1].invert_xaxis()
+ax[1].yaxis.set_label_position("right")
+ax[1].yaxis.tick_right()
+ax[1].set_title("Worst Performing Product", loc="center", fontsize=50)
+ax[1].tick_params(axis='y', labelsize=35)
+ax[1].tick_params(axis='x', labelsize=30)
+ 
+st.pyplot(fig)
+
+#=======================================================================================
+
+#by Negara
+
+fig, ax = plt.subplots(figsize=(20, 10))
+colors = ["#90CAF9", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+sns.barplot(
+    x="customer_count", 
+    y="customer_state",
+    data=bystate_df.sort_values(by="customer_count", ascending=False),
+    palette=colors,
+    ax=ax
+)
+ax.set_title("Number of Customer by States", loc="center", fontsize=30)
+ax.set_ylabel(None)
+ax.set_xlabel(None)
+ax.tick_params(axis='y', labelsize=20)
+ax.tick_params(axis='x', labelsize=15)
+st.pyplot(fig)
+
+#=======================================================================================
+
+#by Tipe Pembayaran
+
+st.subheader("Most payment types")
+fig, ax = plt.subplots(figsize=(20, 10))
+colors = ["#90CAF9", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+sns.barplot(x="payment_type", y="payment_value", data=sum_df.head(5), palette=colors)
+
+st.pyplot(fig)
+
+#=======================================================================================
+
+#RFM
+st.subheader("Best Customer Based on RFM Parameters")
+ 
+col1, col2, col3 = st.columns(3)
+ 
+with col1:
+    avg_recency = round(rfm_df.recency.mean(), 1)
+    st.metric("Average Recency (days)", value=avg_recency)
+ 
+with col2:
+    avg_frequency = round(rfm_df.frequency.mean(), 2)
+    st.metric("Average Frequency", value=avg_frequency)
+ 
+with col3:
+    avg_frequency = format_currency(rfm_df.monetary.mean(), "AUD", locale='es_CO') 
+    st.metric("Average Monetary", value=avg_frequency)
+ 
+fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(35, 15))
+colors = ["#90CAF9", "#90CAF9", "#90CAF9", "#90CAF9", "#90CAF9"]
+ 
+sns.barplot(y="recency", x="customer_id", data=rfm_df.sort_values(by="recency", ascending=True).head(5), palette=colors, ax=ax[0])
+ax[0].set_ylabel(None)
+ax[0].set_xlabel("customer_id", fontsize=30)
+ax[0].set_title("By Recency (days)", loc="center", fontsize=50)
+ax[0].tick_params(axis='y', labelsize=30)
+ax[0].tick_params(axis='x', labelsize=35)
+ 
+sns.barplot(y="frequency", x="customer_id", data=rfm_df.sort_values(by="frequency", ascending=False).head(5), palette=colors, ax=ax[1])
+ax[1].set_ylabel(None)
+ax[1].set_xlabel("customer_id", fontsize=30)
+ax[1].set_title("By Frequency", loc="center", fontsize=50)
+ax[1].tick_params(axis='y', labelsize=30)
+ax[1].tick_params(axis='x', labelsize=35)
+ 
+sns.barplot(y="monetary", x="customer_id", data=rfm_df.sort_values(by="monetary", ascending=False).head(5), palette=colors, ax=ax[2])
+ax[2].set_ylabel(None)
+ax[2].set_xlabel("customer_id", fontsize=30)
+ax[2].set_title("By Monetary", loc="center", fontsize=50)
+ax[2].tick_params(axis='y', labelsize=30)
+ax[2].tick_params(axis='x', labelsize=35)
+ 
+st.pyplot(fig)
+ 
+st.caption('Copyright (c) KEVIN SEP2023')
